df65a2b581f0a1201e50cd41ce03d50c3ffc1a6f
Piotr Halama <halamap@student.mini.pw.edu.pl>
2019-05-22 15:05:29 +0000

    Formatting

--- backend/app/app/db/base.py
+++ backend/app/app/db/base.py
@@ -1,10 +1,22 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
-from app.db.base_class import Base  # noqa
-from app.db_models.user import User  # noqa
+
+from app.db.base_class import Base
 from app.db_models.answer import Answer
 from app.db_models.chart import Chart
 from app.db_models.criterion import Criterion
 from app.db_models.report import Report
 from app.db_models.survey import Survey
 from app.db_models.task import Task
+from app.db_models.user import User
+
+__all__ = [
+    'Base',
+    'Answer',
+    'Chart',
+    'Criterion',
+    'Report',
+    'Survey',
+    'Task',
+    'User'
+]
