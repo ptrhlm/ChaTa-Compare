f0fd0ead95efc92e06e77ddfaab99ccca94fe19b
Probook <wgontarczyk@gmail.com>
2019-05-24 11:16:18 +0200

    Fix task <-> chart relationship

--- backend/app/app/db_models/task.py
+++ backend/app/app/db_models/task.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import relationship
 class Task(Base):
     id = Column(Integer, primary_key=True, index=True)
     chart1_id = Column(Integer, ForeignKey('chart.id'), index=True)
-    chart1 = relationship('Chart', backref='tasks')
+    chart1 = relationship('Chart', backref='tasks_chart_1', foreign_keys=[chart1_id])
     chart2_id = Column(Integer, ForeignKey('chart.id'), index=True)
-    chart2 = relationship('Chart', backref='tasks')
+    chart2 = relationship('Chart', backref='tasks', foreign_keys=[chart2_id])
     survey_id = Column(Integer, ForeignKey('survey.id'), index=True)
-    survey = relationship('Survey', backref='tasks')
+    survey = relationship('Survey', backref='tasks_chart_2')
